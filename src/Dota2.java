4657fca7b664c0c28dcad58f5b92b4b7a2b52027
jon-bassi <jonbassi10@gmail.com>
2014-12-27 11:43:58 -0800

    wrote several methods, added parse options

--- src/Dota2.java
+++ src/Dota2.java
@@ -21,7 +21,9 @@ public class Dota2
    private final String MATCH_INFO_BASE_URL =
          "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=";
    // parse options
-   private int gamemode, minPlayers;
+   private int gamemode = -1;
+   private int minPlayers = -1;
+   private int startAtMatch = -1;
    
    public ArrayList<Integer> matchIDs = new ArrayList<Integer>();
    
@@ -31,11 +33,9 @@ public class Dota2
     * @param gameMode 0-8 for different game modes, 9 if all
     * @param minPlayers 0-10 for minimum number of players per game
     */
-   public Dota2(String apiKey, int gamemode, int minPlayers)
+   public Dota2(String apiKey)
    {
       API_KEY = apiKey;
-      this.gamemode = gamemode;
-      this.minPlayers = minPlayers;
    }
    
    /**
@@ -63,9 +63,28 @@ public class Dota2
       }
    }
    
-   private String addOptionsToURL(String url,int gamemode, int minPlayers)
+   private String addOptionsToURL(String url)
    {
-      return url + "&game_mode=" + gamemode + "&min_players=" + minPlayers;
+      switch (gamemode)
+      {
+      case -1 :
+         break;
+      default : url += "&game_mode=" + gamemode;
+      }
+      switch (minPlayers)
+      {
+      case -1 :
+         break;
+      default : url += "&min_players=" + minPlayers;
+      }
+      
+      switch (startAtMatch)
+      {
+      case -1:
+         break;
+      default : url += "&start_at_match_id=" + startAtMatch;
+      }
+      return url;
    }
    
    /**
@@ -82,15 +101,72 @@ public class Dota2
       return page.replace("body","");
    }
    
-   public void parseManyMatches() throws IOException
+   /**
+    * sets the game mode to parse for, set to -1 to parse all modes
+    * (note: default is set to parse all game modes)
+    * @param type the game mode to parse for
+    */
+   public void setGameMode(int gamemode)
    {
+      this.gamemode = gamemode;
+   }
+   
+   /**
+    * sets minimum number of players per game to parse for, set to -1
+    * to parse for any number of players
+    * (note: default is no preference)
+    * @param minPlayers minimum number of players required per game
+    */
+   public void setMinPlayers(int minPlayers)
+   {
+      this.minPlayers = minPlayers;
+   }
+   
+   /**
+    * sets the match to start at
+    * @param startAtMatch match ID of the match to start parsing at
+    */
+   public void setStartMatch(int startAtMatch)
+   {
+      this.startAtMatch = startAtMatch;
+   }
+   
+   /**
+    * gets the IDs of 100 matches, default method
+    * @param matches the number of matches to parse
+    * @throws IOException
+    */
+   public void getMatchIDs() throws IOException, InterruptedException
+   {
+      Thread.sleep(1500);
       String url = MATCH_HISTORY_BASE_URL;
       url += API_KEY;
-      url = addOptionsToURL(url,gamemode,minPlayers);
+      url = addOptionsToURL(url);
       Document doc = Jsoup.connect(url).ignoreContentType(true).get();
       
       String page = convertToString(doc);
       parseStringFor(page,"match_id:");
+   }
+   
+   /**
+    * gets the IDs of n matches, max is 500
+    * @param matches the number of matches to parse
+    * @throws IOException
+    */
+   public void getMatchIDs(int matches) throws IOException, InterruptedException
+   {
+      String url = MATCH_HISTORY_BASE_URL;
+      url += API_KEY;
+      for (int i = 0; i < matches/100; i++)
+      {
+         System.out.println(i+ 1);
+         url = addOptionsToURL(url);
+         Document doc = Jsoup.connect(url).ignoreContentType(true).get();
+         String page = convertToString(doc);
+         parseStringFor(page,"match_id:");
+         setStartMatch(matchIDs.get(matchIDs.size()-1));
+         Thread.sleep(1500);
+      }
    }
    
    public void parseNextManyMatches() throws IOException, InterruptedException
@@ -98,7 +174,7 @@ public class Dota2
       Thread.sleep(1500);
    }
    
-   public static void parseSingleMatch(Integer matchID) throws IOException, InterruptedException
+   public void parseSingleMatch(Integer matchID) throws IOException, InterruptedException
    {
       /*
       Thread.sleep(1500);
